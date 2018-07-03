ef1b60becc4c4144419e28a902d5becf54370787
Aleksandra <aleksandra.stolyar@gmail.com>
2018-07-03 15:29:28 +0300

    Tidy up markup Add dummy data

--- src/ClientsList.js
+++ src/ClientsList.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import { Grid, List, Image as ImageComponent, Item } from 'semantic-ui-react';
+import {
+  Grid,
+  Header,
+  List,
+  Image, Image as ImageComponent,
+  Item,
+  Input,
+} from 'semantic-ui-react';
 import './App.css';
 import data from './clients.json';
 
-class Search extends Component {
-
+class ClientsSearch extends Component {
+  render() {
+    return (
+      <Input
+        fluid
+        icon='search'
+        iconPosition='right'
+        placeholder='Search users...'
+        className='clients-search'
+      />
+    );
+  }
 }
 
 class ClientsListItem extends React.Component  {
   render() {
     return(
       <Item>
-        <Item.Image src={this.props.client.general.avatar} className='avatar' />
+        <Item.Image
+          src={this.props.client.general.avatar}
+          verticalAlign='top'
+          size='tiny'
+          className='avatar-thumb'
+        />
         <Item.Content>
           <Item.Header as='a'>{this.props.client.general.firstName} {this.props.client.general.lastName}</Item.Header>
           <Item.Meta>
@@ -47,15 +69,28 @@ class ClientsList extends Component {
 class ClientDetails extends Component {
   render() {
     return (
-      <List>
-        <List.Item icon='users' content='Semantic UI' />
-        <List.Item icon='marker' content='New York, NY' />
-        <List.Item
-          icon='mail'
-          content={<a href='mailto:jack@semantic-ui.com'>jack@semantic-ui.com</a>}
-        />
-        <List.Item icon='linkify' content={<a href='http://www.semantic-ui.com'>semantic-ui.com</a>} />
-      </List>
+      <Grid columns={2}>
+        <Grid.Column width={4}>
+          <Image centered size='small' src="https://s3.amazonaws.com/uifaces/faces/twitter/kevinoh/128.jpg" />
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <Header as='h1'>
+            Liana Crooks
+            <Header.Subheader>Investor Functionality Coordinator - Ledner, Johnson and Predovic</Header.Subheader>
+          </Header>
+          <List>
+            <List.Item
+              icon='map marker alternate'
+              content='1520 Zemlak Cove | New Devon | 42586-7898 | Guinea-Bissau'
+            />
+            <List.Item
+              icon='mail'
+              content={<a href='mailto:Gerry_Hackett77@gmail.com'>Gerry_Hackett77@gmail.com</a>}
+            />
+            <List.Item icon='phone' content="(895) 984-0132" />
+          </List>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
@@ -64,10 +99,11 @@ class ClientsPage extends Component {
   render () {
     return (
       <Grid columns={2} divided>
-        <Grid.Column width={4}>
+        <Grid.Column width={4} className='items-list'>
+          <ClientsSearch/>
           <ClientsList/>
         </Grid.Column>
-        <Grid.Column width={12}>
+        <Grid.Column width={12} className='item-details'>
           <ClientDetails/>
         </Grid.Column>
       </Grid>
